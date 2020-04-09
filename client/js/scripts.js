2577e913dc37fb704b3d67b4bd54b51d9f83ec3b
Dewald <Dewald@Dewalds-MacBook-Pro.local>
2020-04-09 09:55:21 +0200

    client server added

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -1,10 +1,4 @@
 const submitForm = (event) => {
   event.preventDefault();
-//   console.log(event.target.elements.fname.value);
-  fetch("http://localhost:3000/post", {
-      method : 'post'
-  })
-  .then(res => res.json())
-  .then(result => console.log(result))
-  .catch(err => console.log(err))
+  console.log(event.target)
 };
